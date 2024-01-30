73bbd5896af3d9fca4670a575b191da32d4bf395
DevWambugu <venusdata100@gmail.com>
2024-01-30 20:49:49 +0300

    first commit

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''0-basic_cache'''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''This class inherits from BaseCaching
+    and is a caching system'''
+    def __init__(self):
+        self.cache_data = {}
+
+    def put(self, key, item):
+        '''Must assign to the dictionary self.cache_data
+        the item value for the key key'''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''Must return the value in
+        self.cache_data linked to key'''
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
