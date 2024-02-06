eab21e25bc69e70d34a3194ce0a91c609e6b8d94
DevWambugu <venusdata100@gmail.com>
2024-02-06 20:49:25 +0300

    install flask babel and configure the app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+class Config:
+    '''This class LANGUAGES class
+    attribute equal to ["en", "fr"]'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+babel = Babel(app)
